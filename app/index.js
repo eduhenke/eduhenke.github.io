4a7d10f7e2ea5dd1dbf4745d7f9a9b618d3719af
Eduardo <eduardo_henke@hotmail.com>
2017-11-25 18:10:49 -0200

    Modify port

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var subdomain = require('express-subdomain');
 var express = require('express');
 var app = express();
-var port = process.env.port || 4000;
+var port = process.env.PORT || 8080;
 
 var router = express.Router();
 router.get('/', require('./local/router.js'))
