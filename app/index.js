ebf29a101529ea93a289795a698ab4454c9ca845
Eduardo <eduardo_henke@hotmail.com>
2017-11-26 18:14:39 -0200

    Add terminal to local./shell

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 var subdomain = require('express-subdomain');
 var express = require('express');
 var app = express();
+var router = express.Router();
+var path    = require("path");
 var port = process.env.PORT || 8080;
 var series = require('middleware-flow').series;
-
+var assetsPath = '/home/eduardo/projects/eduardohenke.com';
 var localIP = '187.65.237.56',
     localPort = 8000,
     request = require('request'),
@@ -24,7 +26,11 @@ function catchConnectionError(err, req, res, next) {
     }
 }
 
-app.use(subdomain('local', series(log, proxy(localURL), catchConnectionError)));
+router.get('/shell', function(req, res){
+    res.sendFile(path.join(__dirname, '/shell/index.html'));
+});
+
+app.use(express.static(assetsPath) ,subdomain('local', series(router, log, proxy(localURL), catchConnectionError)));
 
 app.get('/', function(req, res) {
     res.send('Homepage');
